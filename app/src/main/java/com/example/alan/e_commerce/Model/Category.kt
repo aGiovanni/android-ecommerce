fd5a243b1854f6027e7192fc3b049307827d3114
aGiovanni <alan_giovanni@protonmail.com>
2018-05-22 14:10:15 -0500

    Part 02

--- app/src/main/java/com/example/alan/e_commerce/Model/Category.kt
+++ app/src/main/java/com/example/alan/e_commerce/Model/Category.kt
@@ -1,3 +1,7 @@
 package com.example.alan.e_commerce.Model
 
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
